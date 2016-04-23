be66f2f0c2aae59fe799fe8090f07e4eaef8ead4
Brandon Baris <brandonbaris@Brandons-MacBook-Pro.local>
2016-04-23 10:55:12 -1000

    SCAFFOLDS A BOILERPLATE TOPOJSON-READY LEAFLET MAP APP FOR OUR PLEASURE.

---
+++ app/scripts/main.js
@@ -0,0 +1,25 @@
+/*jslint browser: true*/
+/*global L */
+
+(function (window, document, L, undefined) {
+	'use strict';
+
+	L.Icon.Default.imagePath = 'images/';
+
+	/* create leaflet map */
+	var map = L.map('map', {
+		center: [21.3067, -157.8670],
+		zoom: 4
+	});
+
+	/* add default stamen tile layer */
+	new L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		minZoom: 0,
+		maxZoom: 18,
+		attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+	}).addTo(map);
+
+
+	L.marker([52.5, 13.4]).addTo(map);
+
+}(window, document, L));
